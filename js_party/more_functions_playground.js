8a96d8159077062d5af5c2fdc15f6e333ec2490c
Dustin Peukert <dpeukert04@gmail.com>
2024-11-29 06:18:44 -0500

    Completed

--- js_party/more_functions_playground.js
+++ js_party/more_functions_playground.js
@@ -10,14 +10,34 @@ var cat = {
   weight: "chumby",
 }
 
+function printCatProperties(cat) {
+  console.log(`${cat.name} is ${cat.weight}`)
+}
+
+printCatProperties(cat)
 
 //2. Make a function that takes in a number and returns the corresponding index value from the array below.
      //Create a variable that will hold the string "Mmm, I'm hungry for ______!" <- using an invocation of your function to fill in the blank with a food.
 var foods = ['pizza', 'nachos', 'hot dogs', 'candy'];
 
+function foodDecision(num) {
+  console.log(`Mmm, I'm hungry for ${foods[num]}!`)
+}
+
+foodDecision(0);
+foodDecision(2);
 
 //3. Make a function called truth or dare that takes in one boolean parameter and returns a truth if the boolean is true and a dare if the boolean is false.
+function truthOrDare(boolean) {
+  if (boolean) {
+    console.log('I want to get another snake.')
+  } else {
+    console.log('I dare you to rewrite this file in C#.')
+  }
+}
 
+truthOrDare(true);
+truthOrDare(false);
 
 //4. Check out the rollADie function below which returns a whole number between 1 and whatever number you pass as the argument. Math.floor() is a built-in method that generates a random decimal.  Multiplying by 'num' and adding 1 ensures this random number is between 1 and the 'num' provided. Math.floor() ensures we get a whole number by rounding down to the nearest whole number. Uncomment the console logged invocation of rollADie and run it several times to see the randomly generate numbers.
 
@@ -28,18 +48,59 @@ function rollADie(num) {
   return randomNumber
 }
 
-// console.log(rollADie(6))
+console.log(rollADie(6))
 
 
 //5. Make a function called gambleOnSnakeEyes that creates two variables where each is assigned to an invocation of the rollADie function. If both variables are 1s, then return "You won, congratulations!". Otherwise return "No luck this time, try again!";
+function gambleOnSnakeEyes() {
+  let num1 = rollADie(6);
+  let num2 = rollADie(6);
 
+  if (num1 === 1 && num2 === 1) {
+    console.log('You won, congratulations!');
+  } else {
+    console.log('No luck this time, try again!');
+  }
+}
+
+gambleOnSnakeEyes();
 
 // 6. Create a function called evaluateSalary which takes a parameter of an integer. If the parameter is above 100,000 - the function should return `This is a great salary!`. If the parameter is greater than 75,000 (but less than 100,000) - the function should return `This is a fair salary.` Anything less should return `Time to negotiate!`
+function evaluateSalary(salary) {
+  if (salary > 100000) {
+    console.log('This is a great salary!');
+  } else if (salary < 100000 && salary > 75000) {
+    console.log('This is a fair salary.');
+  } else {
+    console.log('Time to negotiate!');
+  }
+}
 
+evaluateSalary(125000);
+evaluateSalary(76000);
+evaluateSalary(50000);
 
 // 7. Create a function called reassignCounter that takes three parameters, all numbers. Create a locally scoped variable called counter. The function should start by reassigning the variable counter to the first parameter. The newly reassigned counter should then be multiplied by the second parameter. Finally, the counter should be reduced by the third number and that counter should be returned.
+function reassignCounter(num1, num2, num3) {
+  let counter;
+  counter = num1;
+  counter *= num2;
+  counter -= num3;
 
+  return counter;
+}
+
+let counter = reassignCounter(4, 4, 4);
+console.log(counter);
 
 // 8. Create a function called performOps that takes three parameters, all integers - length, width, height. Within your function, create a variable called area that calculates the length x height. Create another variable called volume that calculates length x width x height. Log the volume and return the area. 
+function performOps(length, width, height) {
+  let area = length * width;
+  let volume = length * width * height;
+  console.log(volume);
 
+  return area;
+}
 
+let area = performOps(3, 2, 3);
+console.log(area);
--- js_party/type_coercion_concatenation_interpolation_playground.js
+++ js_party/type_coercion_concatenation_interpolation_playground.js
@@ -7,50 +7,65 @@ console.log("type_coercion_concatenation_interpolation_playground.js is running"
 // PART 1: Type Coercion
 
 // 1a. Look at the variable declarations and assignments below.  Guess what will log for creatureCount.  Uncomment and run it.
-        // var quantity = 3;
-        // var mythicalCreature = " unicorns";
-        // var creatureCount = quantity + mythicalCreature;
-        // console.log(creatureCount)
+        var quantity = 3;
+        var mythicalCreature = " unicorns";
+        var creatureCount = quantity + mythicalCreature;
+        console.log(creatureCount) // I think this will print a string "3 unicorns"
 
 // 1b. In the example above, what is the data type of the quantity variable?
+// an integer
 // 1c. In the example above, what is the data type of the mythicalCreature variable?
+// a string
 // 1d. In the example above, what is the data type of the creatureCount variable?  
-//     How can a number plus a string equal a string?
+// a string
+//     How can a number plus a string equal a string? by adding them together the integer 3 is converted to a string character 3 since the + operand expects numbers
 //     Take a few minutes to look up and read about Type Coercion in the MDN docs.
 
 
 
 
 // PART 2: Concatenation
-  // var brag = "I have " + quantity + " fancy" + mythicalCreature +"!"
+  var brag = "I have " + quantity + " fancy" + mythicalCreature +"!"
 
 // 2a. Ensure the quantity, mythicalCreature, and brag variables above are not commented out.  Click Run.
-
+  console.log(brag);
 // 2b. The value of brag above is being created using concatenation. A way to link multiple values (strings, numbers, variables, etc) together into one string using the + operator.  Let's try it!
    // Declare a variable and assign it a string value of a friend's name.
+   let friendName = 'Avery';
    // Declare a variable and assign it a string value of a nice gesture.
+   let gesture = 'reminding me to take out the trash';
    // Declare a variable "gratitude" and assign it a statement of thanks that uses concatenation and your other 2 variables.  For example:  "Thank you Kayla for pointing out the food in my teeth."
+   let gratitude = 'Thank you ' + friendName + ' for ' + gesture + '.';
+   console.log(gratitude);
 
 // 2c. Use the variables below to make a concatenated string.
-      // var activity = 'talk'
-      // var estrangedUncle = 'Bruno'
-
-
+      var activity = 'talk'
+      var estrangedUncle = 'Bruno'
+      let statement = "We don't " + activity + " about " + estrangedUncle + "!"
+      console.log(statement);
 
 
 // PART 3: Interpolation
-  // var bigBrag = `I have ${quantity} VERY fancy and sophisticated${mythicalCreature}!`  
+  var bigBrag = `I have ${quantity} VERY fancy and sophisticated${mythicalCreature}!`  
         //Note the weird spacing between sophisticated and mythicalCreature above due to how we already included a space in the mythicalCreature value previously.
 
 // 3a. Ensure the quantity, mythicalCreature, and bigBrag variables above are not commented out. Click Run.
+console.log(bigBrag);
 
 // 3b. The bigBrag variable above uses a different way of combining values called interpolation. It accomplishes the same goal as concatenation but uses different syntax - backticks instead of quotes(``) and these guys ${}.  Let's try it!
      // Declare a variable and assign it a string value of a name.
+     let name = 'Mason';
      // Declare a variable and assign it a string value of a celebratory exclamation ("congrats", "woohoo", etc)
+     let exclamation = 'Yay';
      // Declare a variable and assign it to a string value of a nice characteristic ("cute", "nice", etc)
+     let characteristic = 'cool';
      // Declare a variable "babyNote" and assign it a statement of new baby congratulations that uses interpolation and your other 3 variables.  For example: "Congratulations Trisha! I heard your new baby is adorable!"
+     let babyNote = `${exclamation} ${name}! I heard your new baby is ${characteristic}!`
+     console.log(babyNote);
 
 // 3c. Use the variables below to make an interpolated string.
-      // var celebration = 'birthday'
-      // var age = 25
+      var celebration = 'birthday';
+      var age = 25;
+      let birthdayStatement = `Happy ${celebration}! You're ${age} now!`;
+      console.log(birthdayStatement);
 
